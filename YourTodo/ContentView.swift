58e6d4a35f64aa87c839e55c313ffd04e373244a
amillerr <Axenov Artem>
2022-08-26 17:49:45 +0300

    Initial Commit

---
+++ YourTodo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YourTodo
+//
+//  Created by Artem Axenov on 2022-08-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YourTodo/YourTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourTodoApp.swift
+//  YourTodo
+//
+//  Created by Artem Axenov on 2022-08-26.
+//
+
+import SwiftUI
+
+@main
+struct YourTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
